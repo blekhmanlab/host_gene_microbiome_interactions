f6c9c24b2667ce3b2f31cc8c634fc0bbf2871255
Sambhawa Priya <sambhawa.priya@gmail.com>
2021-09-08 23:07:34 -0500

    Update sparseCCA_tutorial.R
    Added check to install packages if not installed and load them

--- Tutorial/sparseCCA_tutorial/sparseCCA_tutorial.R
+++ Tutorial/sparseCCA_tutorial/sparseCCA_tutorial.R
@@ -1,8 +1,17 @@
 ## Sparse CCA tutorial
 
-## import libraries
-library(PMA)
-library(data.table)
+## install and import libraries
+
+## Install and import libraries
+check.packages <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE, repos = "http://cran.us.r-project.org")
+  sapply(pkg, require, character.only = TRUE)
+}
+
+packages <- c("PMA","data.table") 
+check.packages(packages)
 
 ################# Functions ################
 
